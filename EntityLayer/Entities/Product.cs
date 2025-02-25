5ccbb08414d56653c7cea3650fa7e99dc701fd89
Emirhan Eren <emirhanneren52@outlook.com>
2025-02-25 21:06:45 +0300

    fix Prodcut dto and Product name type

--- EntityLayer/Entities/Product.cs
+++ EntityLayer/Entities/Product.cs
@@ -3,7 +3,7 @@
     public class Product
     {
         public int ProductID { get; set; }
-        public int ProductName { get; set; }
+        public string ProductName { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
