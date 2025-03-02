5526ef32a6398387f8efc4073574c0dc25e963ba
Emirhan Eren <emirhanneren52@outlook.com>
2025-03-02 17:36:01 +0300

    Order Entitiy Added

---
+++ EntityLayer/Entities/OrderDetail.cs
@@ -0,0 +1,14 @@
+﻿namespace EntityLayer.Entities
+{
+    public class OrderDetail
+    {
+        public int OrderDetailID { get; set; }
+        public int ProductID { get; set; }
+        public Product Product { get; set; }
+        public decimal Count { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int OrderID { get; set; }
+        public Order Order { get; set; }
+    }
+}
