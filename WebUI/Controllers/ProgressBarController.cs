322c9db44a274809a39929a93b21f976f7c891ad
Emirhan Eren <emirhanneren52@outlook.com>
2025-03-05 22:04:01 +0300

    Progress Page added

--- WebUI/Controllers/ProgressBarController.cs
+++ WebUI/Controllers/ProgressBarController.cs
@@ -2,13 +2,9 @@
 
 namespace WebUI.Controllers
 {
-    public class SignalRDefaultController : Controller
+    public class ProgressBarController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
-        }
-        public IActionResult Index2()
         {
             return View();
         }
