675103ae68f5afb91a6838b5df91169f3f87d9ee
Emirhan Eren <emirhanneren52@outlook.com>
2025-03-08 19:06:24 +0300

    UI design added and init

---
+++ WebUI/ViewComponents/UILayoutComponents/_UILayoutSliderPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutSliderPartialComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
