f88ad9d927ac6a730347b7787bedc85527e4bd84
Emirhan Eren <emirhanneren52@outlook.com>
2025-03-21 18:06:42 +0300

    user information edit page

---
+++ WebUI/Controllers/SettingsController.cs
@@ -0,0 +1,47 @@
+﻿using EntityLayer.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.Dtos.IdentityDtos;
+
+namespace WebUI.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public SettingsController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var value = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditDto userEditDto = new UserEditDto()
+            {
+                Surname = value.Surname,
+                Name = value.Name,
+                Username = value.UserName,
+                Mail = value.Email
+            };
+            return View(userEditDto);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditDto userEditDto)
+        {
+            if (userEditDto.Password == userEditDto.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.Name = userEditDto.Name;
+                user.Surname = userEditDto.Surname;
+                user.Email = userEditDto.Mail;
+                user.UserName = userEditDto.Username;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, userEditDto.Password);
+                await _userManager.UpdateAsync(user);
+                return RedirectToAction("Index", "Statistic");
+            }
+            return View();
+        }
+    }
+}
