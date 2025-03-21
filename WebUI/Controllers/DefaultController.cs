6d2dc45047a0ce0c3884f3348a1ad172083fb701
Emirhan Eren <emirhanneren52@outlook.com>
2025-03-21 18:54:22 +0300

    logout added

--- WebUI/Controllers/DefaultController.cs
+++ WebUI/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Text;
 using WebUI.Constants;
@@ -6,6 +7,7 @@ using WebUI.Dtos.MessageDtos;
 
 namespace WebUI.Controllers
 {
+    [AllowAnonymous]
     public class DefaultController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
--- WebUI/Controllers/LoginController.cs
+++ WebUI/Controllers/LoginController.cs
@@ -30,5 +30,10 @@ namespace WebUI.Controllers
             }
             return View();
         }
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index","Default");
+        }
     }
 }
