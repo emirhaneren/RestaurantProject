6be867bac547771524432a19653bee525ac86eb1
Emirhan Eren <emirhanneren52@outlook.com>
2025-03-21 16:22:36 +0300

    qr- added

---
+++ WebUI/Controllers/QrCodeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace WebUI.Controllers
+{
+    public class QrCodeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string value)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+                QRCodeGenerator.QRCode qRCode = qRCodeGenerator.CreateQrCode(value, QRCodeGenerator.ECCLevel.Q);
+                using (Bitmap image = qRCode.GetGraphic(20))
+                {
+                    image.Save(memoryStream, ImageFormat.Png);
+                    ViewBag.QrCodeImage = "data:image/png;base64," + Convert.ToBase64String(memoryStream.ToArray());
+                }
+            }
+            return View();
+        }
+    }
+}
