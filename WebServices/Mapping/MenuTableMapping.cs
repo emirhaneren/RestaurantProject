12762f535825d3d458af1782aa07aa9ae29fe09b
Emirhan Eren <emirhanneren52@outlook.com>
2025-03-22 16:33:23 +0300

    AutoMapper added

---
+++ WebServices/Mapping/MenuTableMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DtoLayer.MenuTableDto;
+using EntityLayer.Entities;
+
+namespace WebServices.Mapping
+{
+    public class MenuTableMapping:Profile
+    {
+        public MenuTableMapping()
+        {
+            CreateMap<MenuTable, ResultMenuTableDto>().ReverseMap();
+            CreateMap<MenuTable, GetMenuTableDto>().ReverseMap();
+            CreateMap<MenuTable, UpdateMenuTableDto>().ReverseMap();
+            CreateMap<MenuTable, CreateMenuTableDto>().ReverseMap();
+        }
+    }
+}
---
+++ WebServices/Mapping/NotificationMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DtoLayer.NotificationDto;
+using EntityLayer.Entities;
+
+namespace WebServices.Mapping
+{
+    public class NotificationMapping:Profile
+    {
+        public NotificationMapping()
+        {
+            CreateMap<Notification,ResultNotificationDto>().ReverseMap();
+            CreateMap<Notification, GetNotificationDto>().ReverseMap();
+            CreateMap<Notification, UpdateNotificationDto>().ReverseMap();
+            CreateMap<Notification, CreateNotificationDto>().ReverseMap();
+        }
+    }
+}
