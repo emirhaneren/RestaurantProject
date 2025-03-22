559bb22f1e94cd074c37436fdf0ec4f190861b4b
Emirhan Eren <emirhanneren52@outlook.com>
2025-03-22 16:08:15 +0300

    404 page added

---
+++ WebUI/Controllers/ErrorPageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorPageController : Controller
+    {
+        public IActionResult NotFound404()
+        {
+            return View();
+        }
+    }
+}
