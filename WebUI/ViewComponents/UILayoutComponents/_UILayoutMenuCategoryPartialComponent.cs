30cb5a03eafd9f9d54ea7e9837d9055633bfa9b1
Emirhan Eren <emirhanneren52@outlook.com>
2025-03-22 18:02:00 +0300

    Category problem fixed for menu pages

---
+++ WebUI/ViewComponents/UILayoutComponents/_UILayoutMenuCategoryPartialComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebUI.Constants;
+using WebUI.Dtos.CategoryDtos;
+
+namespace WebUI.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutMenuCategoryPartialComponent:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _UILayoutMenuCategoryPartialComponent(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMsg = await client.GetAsync(WebServiceAdresses.categoryApi);
+            if (responseMsg.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMsg.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
